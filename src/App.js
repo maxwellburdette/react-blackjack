137c16bf72e046d01f3433d827c425e7b8df98d9
maxwellburdette <maxbdevelops@gmail.com>
2021-05-04 02:27:31 -0500

    Cycles deck

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./components/Card";
 function App() {
 	const [deck, setDeck] = useState([]);
 	const color = useRef("red");
 	const symbol = useRef(["fab", "steam"]);
+	const random = useRef(0);
+	const [cardID, setCardID] = useState(0);
+	const [card, setCard] = useState({
+		color: "",
+		symbol: [],
+		value: "",
+	});
 
 	useEffect(() => {
 		generateDeck();
@@ -11,30 +18,99 @@ function App() {
 		// eslint-disable-next-line
 	}, []);
 
-	function generateDeck() {
-		for (let i = 1; i <= 52; i++) {
-			if (i == 13) {
-				color.current = "black";
-				symbol.current = ["fab", "discord"];
-			} else if (i == 37) {
-				color.current = "black";
-				symbol.current = ["fab", "battle-net"];
-			} else if (i == 25) {
-				color.current = "red";
-				symbol.current = ["fab", "windows"];
+	useEffect(() => {
+		getCard();
+	}, []);
+
+	async function generateDeck() {
+		// for (let i = 1; i <= 52; i++) {
+		// 	setDeck((prevState) => [
+		// 		...prevState,
+		// 		{ value: i, cardColor: color.current, symbol: symbol.current },
+		// 	]);
+		// }
+
+		//Generate first suit
+		generateSuit(color.current, symbol.current);
+
+		//Change references
+		color.current = "#111";
+		symbol.current = ["fab", "discord"];
+		//Generate second suit
+		generateSuit(color.current, symbol.current);
+
+		//Change references
+		color.current = "red";
+		symbol.current = ["fab", "battle-net"];
+		//Generate thrid suit
+		generateSuit(color.current, symbol.current);
+
+		//Change references
+		color.current = "#111";
+		symbol.current = ["fab", "windows"];
+		generateSuit(color.current, symbol.current);
+
+		//random.current = randomNumber(0, 51);
+	}
+
+	function randomNumber(start, end) {
+		return Math.floor(Math.random() * end) + start;
+	}
+
+	function generateSuit(color, symbol, suitID) {
+		for (let i = 1; i <= 13; i++) {
+			setCardID();
+			let value = i;
+			if (i === 1) {
+				value = "A";
 			}
 
-			console.log(color.current);
+			if (i === 11) {
+				value = "J";
+			}
+			if (i === 12) {
+				value = "Q";
+			}
+			if (i === 13) {
+				value = "K";
+			}
 			setDeck((prevState) => [
 				...prevState,
-				{ value: i, cardColor: color.current, symbol: symbol.current },
+				{
+					value: value,
+					cardColor: color,
+					symbol: symbol,
+					number: i,
+					cardID: cardID,
+				},
 			]);
 		}
+	}
+
+	function getCard(e) {
+		console.log("Clicked");
+		random.current = randomNumber(0, deck.length);
+		let thisCard = deck[random.current];
+		if (thisCard === undefined) return;
+		setDeck(deck.filter((removeCard) => removeCard.cardID !== deck.cardID));
+		setCard({
+			color: thisCard.cardColor,
+			value: thisCard.value,
+			symbol: thisCard.symbol,
+		});
 	}
 	//<Card value={"A"} symbol={["fab", "steam"]}></Card>
 	//<Card value={2} symbol={["fab", "discord"]} color={"#111"}></Card>
 	//<Card value={3} symbol={["fab", "battle-net"]} color={"#111"}></Card>
 	//<Card value={4} symbol={["fab", "windows"]}></Card>
+	// {deck.map((card) => (
+	// 	<Card
+	// 		value={card.value}
+	// 		color={card.cardColor}
+	// 		symbol={card.symbol}
+	// 	></Card>
+	// ))}
+	//<>{deck.length > 0 && getCard()}</>
 	return (
 		<div className="App">
 			<h1
@@ -46,13 +122,9 @@ function App() {
 			>
 				Blackjack
 			</h1>
-			{deck.map((card) => (
-				<Card
-					value={card.value}
-					color={card.cardColor}
-					symbol={card.symbol}
-				></Card>
-			))}
+			<div onClick={getCard}>
+				<Card value={card.value} color={card.color} symbol={card.symbol}></Card>
+			</div>
 		</div>
 	);
 }
