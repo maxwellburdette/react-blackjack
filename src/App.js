699a653bb233a9c37fc236a50ec5685abd227b83
maxwellburdette <maxbdevelops@gmail.com>
2021-05-03 23:28:09 -0500

    First Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect, useRef } from "react";
+import Card from "./components/Card";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [deck, setDeck] = useState([]);
+	const color = useRef("red");
+	const symbol = useRef(["fab", "steam"]);
+
+	useEffect(() => {
+		generateDeck();
+
+		// eslint-disable-next-line
+	}, []);
+
+	function generateDeck() {
+		for (let i = 1; i <= 52; i++) {
+			if (i == 13) {
+				color.current = "black";
+				symbol.current = ["fab", "discord"];
+			} else if (i == 37) {
+				color.current = "black";
+				symbol.current = ["fab", "battle-net"];
+			} else if (i == 25) {
+				color.current = "red";
+				symbol.current = ["fab", "windows"];
+			}
+
+			console.log(color.current);
+			setDeck((prevState) => [
+				...prevState,
+				{ value: i, cardColor: color.current, symbol: symbol.current },
+			]);
+		}
+	}
+	//<Card value={"A"} symbol={["fab", "steam"]}></Card>
+	//<Card value={2} symbol={["fab", "discord"]} color={"#111"}></Card>
+	//<Card value={3} symbol={["fab", "battle-net"]} color={"#111"}></Card>
+	//<Card value={4} symbol={["fab", "windows"]}></Card>
+	return (
+		<div className="App">
+			<h1
+				style={{
+					width: "100%",
+					textAlign: "center",
+					margin: "10px 15px",
+				}}
+			>
+				Blackjack
+			</h1>
+			{deck.map((card) => (
+				<Card
+					value={card.value}
+					color={card.cardColor}
+					symbol={card.symbol}
+				></Card>
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Card(props) {
+	return (
+		<div className="cardContainer" style={{ color: props.color }}>
+			<div className="number top">{props.value}</div>
+			<div className="smallSuit left">
+				<FontAwesomeIcon icon={props.symbol} size="lg"></FontAwesomeIcon>
+			</div>
+			<div className="smallSuit bottom-right">
+				<FontAwesomeIcon icon={props.symbol} size="lg"></FontAwesomeIcon>
+			</div>
+			<div className="number bottom">{props.value}</div>
+			<div className="largeSuit">
+				<FontAwesomeIcon
+					className="icon"
+					icon={props.symbol}
+					size="3x"
+				></FontAwesomeIcon>
+			</div>
+			<div className="largeSuit">
+				<FontAwesomeIcon
+					className="icon-bottom"
+					icon={props.symbol}
+					size="3x"
+				></FontAwesomeIcon>
+			</div>
+		</div>
+	);
+}
